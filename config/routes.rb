94fbf2177b01b9a4505d7f8a8dc6ad8831489ac0
littleali <ali.nosrati72@gmail.com>
2015-07-11 16:07:56 +0430

    Patient scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :patients
+
   resources :profiles
 
   get 'welcome/index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711113256) do
+ActiveRecord::Schema.define(version: 20150711113651) do
+
+  create_table "patients", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "profiles", force: true do |t|
     t.string   "first_name"
