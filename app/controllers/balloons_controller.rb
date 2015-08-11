ae34a2f81cf7b471dc7114c3c29ec9e9fe4b2edb
Une30 <younesi429@yahoo.com>
2015-08-11 16:57:35 +0430

    Bart logic changed(balloons are now being edited instead of being created)

--- app/controllers/balloons_controller.rb
+++ app/controllers/balloons_controller.rb
@@ -1,35 +1,10 @@
 class BalloonsController < ApplicationController
-  before_action :set_balloon, only: [:show, :edit, :update, :destroy]
+  before_action :set_balloon
   before_filter :authenticate_user!
   respond_to :html
 
-  def index
-    @balloons = Balloon.all
-    respond_with(@balloons)
-  end
-
-  def show
-    respond_with(@balloon)
-  end
-
-  def new
-    @balloon = Balloon.new
-    respond_with(@balloon)
-  end
-
-  def create
-    @balloon = Balloon.new(balloon_params)
-    @balloon.save
-    respond_with(@balloon)
-  end
-
   def update
     @balloon.update(balloon_params)
-    respond_with(@balloon)
-  end
-
-  def destroy
-    @balloon.destroy
     respond_with(@balloon)
   end
 
